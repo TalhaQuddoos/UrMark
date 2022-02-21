20bb9d971f82c41d25313e4b4b2284a2a867e820
Talha Quddoos <talhaquddoos1@gmail.com>
2022-02-22 02:50:49 +0500

    change port

--- app.js
+++ app.js
@@ -35,6 +35,8 @@ app.post("/convert", upload.single("google-doc"), (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("App listening on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
 });
